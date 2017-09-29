f35f8ad4b89ef5d73f8473b4968167cc0a4dabc7
Manish Gotame <hackrax123456789@gmail.com>
2017-09-29 08:58:42 +0545

    Delete falcon.py

--- falcon.py
+++
@@ -1,6 +0,0 @@
-import keras 
-from keras.models import Sequential
-from keras.layers import Dense , Activation, Flatten,Dropout 
-from keras.datasets import mnist 
-
-(X_train, y_train),(X_test, y_test) = mnist.load_data()
